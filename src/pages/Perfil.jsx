df1ec818006d6fb81cb065f0e023db345374f569
FranciscoMilena <franciscvillonpico@gmail.com>
2025-07-27 21:28:22 -0500

    Update Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import "../styles/Perfil.css";
+import "../styles/perfil.css";
 
 export default function Perfil() {
   const [usuario, setUsuario] = useState(null);
